acb429a877f10a51174fb531cd72520b5bbe3fe8
ilhan enes daniş <danisenes3@gmail.com>
2022-08-30 21:06:00 +0300

    Add project files.

---
+++ ConsoleDocker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleDocker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i<1000)
+            {
+                Console.WriteLine("Hello World "+i);
+                System.Threading.Thread.Sleep(500);
+                i++;
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
